10f95a03ba235e0691842b5b86cf9f326fd827c2
PostnikovDenis24 <postnikovdenis1@gmail.com>
2024-10-27 15:05:58 +0300

    Лабораторная работа 2

---
+++ лаба 2/task_1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+month = 0
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+while True:
+     razn = spend - salary
+     if razn > money_capital:
+       break
+         
+     month += 1
+     money_capital -= razn
+     spend *= 1 + increase
+
+
+print("Количество месяцев, которое можно протянуть без долгов:", month)
